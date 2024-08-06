7c3d08a368216b409eaff193e2c7f17d773a9a20
PriyaWebDesigner <naskartanu@111gmail.com>
2024-08-06 16:24:09 +0530

    DOM

--- script.js
+++ script.js
@@ -12,7 +12,17 @@ function sum(){
     let result = parseFloat(x)+parseFloat(y);
 
     document.getElementById('result').innerHTML = 'The result is =' + result;
-    console.log('The result is =' + result);
+    // console.log('The result is =' + result);
 }
-
+sum();
 // console.log('vegeg'+'ewgwfg')
+
+function multiply(){
+    event.preventDefault();
+    let a = document.getElementById('price1').value;
+    let b = document.getElementById('price2').value;
+
+    let result = parseFloat(a)+parseFloat(b);
+    document.getElementById('totalPrice').innerhtml = 'The Total Price is= '+totalPrice;
+}
+multiply();
